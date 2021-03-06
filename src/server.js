996d37c20c5817c3e34de77c5ff17f4feec1707c
Rafic El Kouche <raficelkouche@hotmail.com>
2021-03-06 01:12:45 -0500

    refactor server module to take care of the connection only

--- src/server.js
+++ src/server.js
@@ -1,28 +1,5 @@
+const app = require('./application')
 const PORT = process.env.PORT || 8001;
-const ENV = require("dotenv").config();
-const express = require("express");
-const cookieSession = require("cookie-session");
-const methodOverride = require("method-override");
-const app = express();
-const db = require("../db/helper")
-
-app.use(cookieSession({
-  name: 'session',
-  keys: ['secret1', 'secret2']
-}));
-
-app.use(express.urlencoded({ extended: true }));
-
-//allow for PUT and DELETE requests
-app.use(methodOverride('_method'));
-
-//define and mount the routes
-const userRoutes = require("./routes/userRoutes");
-const tweetRoutes = require("./routes/tweetRoutes");
-const sessionRoutes = require("./routes/sessionRoutes");
-app.use("/api/users", userRoutes(db));
-app.use("/api/tweets", tweetRoutes(db));
-app.use("/api/sessions", sessionRoutes(db));
 
 //start the server
 app.listen(PORT, () => {

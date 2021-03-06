02df8e09d7dc45f4556c347fe8a04ce57d2d5eca
Rafic El Kouche <raficelkouche@hotmail.com>
2021-03-06 01:12:25 -0500

    move the express app logic to a separate module

---
+++ src/application.js
@@ -0,0 +1,28 @@
+const ENV = require("dotenv").config();
+const express = require("express");
+const cookieSession = require("cookie-session");
+const methodOverride = require("method-override");
+const app = express();
+const db = require("../db/helper")
+
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['secret1', 'secret2']
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+//allow for PUT and DELETE requests
+app.use(methodOverride('_method'));
+
+//define and mount the routes
+const userRoutes = require("./routes/userRoutes");
+const tweetRoutes = require("./routes/tweetRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+app.use("/api/users", userRoutes(db));
+app.use("/api/tweets", tweetRoutes(db));
+app.use("/api/sessions", sessionRoutes(db));
+
+module.exports = app

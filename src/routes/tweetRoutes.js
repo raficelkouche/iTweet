b96a6448fffb96ea94306fa43d1e238bdf3f7797
Rafic El Kouche <raficelkouche@hotmail.com>
2021-03-07 04:46:47 -0500

    define GET /tweets

--- src/routes/tweetRoutes.js
+++ src/routes/tweetRoutes.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
+const db = require('../../db/helper');
 
 module.exports = db => {
   router.get("/", (req, res) => {
-    res.json({ "name": "Tweets" })
+    const userID = req.session.userID
+    
+    if(userID) {
+      db.getAllTweets()
+        .then(data => {
+          res.status(200).json(data)
+        })
+    } else {
+        res.status(500).json({error: "please login first"})
+    }
   })
-
   return router
 }

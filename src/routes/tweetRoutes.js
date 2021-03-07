39c6becb16269cb551f493324a81245e13e5c246
Rafic El Kouche <raficelkouche@hotmail.com>
2021-03-07 12:16:11 -0500

    add endpoints to support like/unlike

--- src/routes/tweetRoutes.js
+++ src/routes/tweetRoutes.js
@@ -14,5 +14,34 @@ module.exports = db => {
         res.status(500).json({error: "please login first"})
     }
   })
+  
+  router.put("/:tweet_id/like", (req, res) => {
+    const userID = req.session.userID 
+    let tweet_id = req.params.tweet_id
+
+    if(userID) {
+      db.likeTweet(tweet_id)
+        .then(data => {
+          data ? res.status(200).json(data) : res.status(500).json({error: "tweet not found"})
+        })
+    } else {
+      res.status(500).json({ error: "unauthorized" })
+    }
+  })
+
+  router.put("/:tweet_id/unlike", (req, res) => {
+    const userID = req.session.userID
+    let tweet_id = req.params.tweet_id
+
+    if (userID) {
+      db.unlikeTweet(tweet_id)
+        .then(data => {
+          data ? res.status(200).json(data) 
+          : res.status(500).json({ error: "tweet not found / likes can't be negative" })
+        })
+    } else {
+      res.status(500).json({ error: "unauthorized" })
+    }
+  })
   return router
 }

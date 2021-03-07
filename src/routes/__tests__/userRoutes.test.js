3377ad9c0804f7ecbf5dd392207a4107e32f340b
Rafic El Kouche <raficelkouche@hotmail.com>
2021-03-07 10:24:52 -0500

    test the tweets routes

--- src/routes/__tests__/userRoutes.test.js
+++ src/routes/__tests__/userRoutes.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const app  = require("../../application");
-const { closeConnection } = require("../../../db/helper")
+const { closeConnection, getTweetByID, getAllTweets } = require("../../../db/helper")
 
 describe("User Registration, /users endpoint", () => {
   
@@ -33,9 +33,9 @@ describe("User Registration, /users endpoint", () => {
     expect(response.body).toHaveProperty('error', 'access denied!')
   })
   
-  const agent = request.agent(app); //using an agent to be able to store the session cookies for the next test
+  const agent = request.agent(app); //using an agent to be able to store the session cookies for the next tests
   let user_id; //to be used by the next test
-  test("users should be logged in to be able to retrieve their messages", async () => {
+  test("logged in users will have session cookies set", async () => {
     await agent
       .post('/api/sessions')
       .send({ username: "Rafic", password: "123456" })
@@ -53,5 +53,108 @@ describe("User Registration, /users endpoint", () => {
       .get(`/api/users/${user_id}/messages`)
     expect(response.statusCode).toBe(200)
     expect(response.body).not.toHaveLength(0)
+  })
+
+  test("authenticated users can retrieve all their tweets", async () => {
+    await agent
+      .get(`/api/users/${user_id}/tweets`)
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+        expect.objectContaining({
+          id: expect.any(Number),
+          text: expect.any(String)
+        })
+      })
+  })
+
+  test("an error should occur if user 1 tries to retrieve user 2's tweets", async () => {
+    await agent
+      .get('/api/users/2/tweets')
+      .then(response => {
+        expect(response.statusCode).toBe(500)
+        expect(response.body).toHaveProperty('error', 'access denied!')
+        expect(response.body).not.toHaveProperty('id')
+      })
+  })
+
+  test("users should be able to retrieve a specific tweet", async () => {
+    const tweet_id = 1; //this tweet belongs to user_id 1
+    await agent
+      .get(`/api/users/${user_id}/tweets/${tweet_id}`)
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+        expect.objectContaining({
+          id: tweet_id,
+          user_id
+        })
+      })
+  })
+
+  test("if a tweet is not found or does not belong to the user, an error will be returned", async () => {
+    const tweet_id = 3; //this tweet belongs to user_id 2
+    await agent
+      .get(`/api/users/${user_id}/tweets/${tweet_id}`)
+      .then(response => {
+        expect(response.statusCode).toBe(500)
+      })
+  })
+
+  test("if a tweet belongs to a user, they should be able to edit it", async () => {
+    const newTweet = "This is for testing purposes"
+    const tweet_id = 2; //this tweet belongs to user_id 1
+    await agent
+      .put(`/api/users/${user_id}/tweets/${tweet_id}`)
+      .send({tweet: newTweet})
+      .set('Accept', 'application/x-www-form-urlencoded')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect.objectContaining({
+          text: newTweet
+        })
+      })
+  })
+
+  test("if a user tries to edit a tweet that doesn't belong to them, an error should be returned", async () => {
+    const newTweet = "This is for testing purposes"
+    const tweet_id = 3; //this tweet does not belong to user_id 1
+    await agent
+      .put(`/api/users/${user_id}/tweets/${tweet_id}`)
+      .send({ tweet: newTweet })
+      .set('Accept', 'application/x-www-form-urlencoded')
+      .then(response => {
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toHaveProperty('error')
+      })
+  })
+  
+  test("should delete a tweet if the tweet owner requests it", async () => {
+    const tweet_id = 2; //this tweet belongs to user_id 1
+    await agent
+      .delete(`/api/users/${user_id}/tweets/${tweet_id}`)
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+      })
+    //call DB and confirm deletion
+    await getTweetByID(user_id, tweet_id)
+      .then(data => {
+        expect(data).not.toBeDefined()
+      })
+  })
+
+  test("if an unauthorized user tries to delete a tweet, an error is received", async () => {
+    const tweet_id = 3; //this tweet doest not belong to user_id 1
+    await agent
+      .delete(`/api/users/${user_id}/tweets/${tweet_id}`)
+      .then(response => {
+        expect(response.statusCode).toBe(500)
+        expect(response.body).toHaveProperty('error')
+      })
+    //call DB and confirm that no deletion took place
+    await getAllTweets()
+      .then(() => {
+        expect.objectContaining({
+          id: tweet_id
+        })
+      })
   })
 })

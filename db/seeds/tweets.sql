274b97975827c04ca9ea7dd72684444016bf8d5e
Rafic El Kouche <raficelkouche@hotmail.com>
2021-03-07 04:46:28 -0500

    seed the tweets table

--- db/seeds/tweets.sql
+++ db/seeds/tweets.sql
@@ -1,1 +1,4 @@
-INSERT INTO tweets (text, user_id) VALUES ('I have just joined iTweet!', 1)
+INSERT INTO tweets (text, user_id) VALUES ('I have just joined iTweet!', 1);
+INSERT INTO tweets (text, user_id) VALUES ('Anyone up for lunch?', 1);
+INSERT INTO tweets (text, user_id) VALUES ('I have my first interview today, wish me luck guys...', 2);
+INSERT INTO tweets (text, user_id) VALUES ('There will be a talk by a software engineer from Amazon at 3:00PM EST make sure you check it out!', 3);

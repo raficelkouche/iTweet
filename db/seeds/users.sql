e71ab13441b346d8e9b345eb67a1c68e4ba6d8d1
Rafic El Kouche <raficelkouche@hotmail.com>
2021-03-06 20:36:46 -0500

    add more users

--- db/seeds/users.sql
+++ db/seeds/users.sql
@@ -1,1 +1,3 @@
-INSERT INTO users (username, password) VALUES ('rafic', '$2b$10$TxdnHU2hjro8Xo9wLjDvruEDjkhLcUsjpJ7MB8W9zmwkx8t0M03ki');
+INSERT INTO users (username, password) VALUES ('Rafic', '$2b$10$TxdnHU2hjro8Xo9wLjDvruEDjkhLcUsjpJ7MB8W9zmwkx8t0M03ki');
+INSERT INTO users (username, password) VALUES ('John', '$2b$10$TxdnHU2hjro8Xo9wLjDvruEDjkhLcUsjpJ7MB8W9zmwkx8t0M03ki');
+INSERT INTO users (username, password) VALUES ('Adam', '$2b$10$TxdnHU2hjro8Xo9wLjDvruEDjkhLcUsjpJ7MB8W9zmwkx8t0M03ki');

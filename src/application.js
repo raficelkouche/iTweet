931762a47af460cd524147f82e2cfe76c4caccd0
Rafic El Kouche <raficelkouche@hotmail.com>
2021-03-08 13:31:10 -0500

    code linting

--- src/application.js
+++ src/application.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cookieSession = require("cookie-session");
 const methodOverride = require("method-override");
 const app = express();
-const db = require("../db/helper")
+const db = require("../db/helper");
 
 
 app.use(cookieSession({
@@ -12,7 +12,7 @@ app.use(cookieSession({
 }));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 //allow for PUT and DELETE requests
 app.use(methodOverride('_method'));
@@ -25,4 +25,4 @@ app.use("/api/users", userRoutes(db));
 app.use("/api/tweets", tweetRoutes(db));
 app.use("/api/sessions", sessionRoutes(db));
 
-module.exports = app
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -8,40 +8,40 @@ const { v4: uuidv4 } = require('uuid');
 const activeConnections = {}; //This is to keep track of all connected clients
 
 wss.on('connection', socket => {
-  const clientID = socket.handshake.query.id || uuidv4()  //expect client to pass their ID
-  const clientName = socket.handshake.query.username || "username" //expect client to pass their username
-  const userInfo = { clientID, clientName }
+  const clientID = socket.handshake.query.id || uuidv4();  //expect client to pass their ID
+  const clientName = socket.handshake.query.username || "username"; //expect client to pass their username
+  const userInfo = { clientID, clientName };
   const conversation = {}; //store all conversations for this client in memory and then push them to the db once they disconnect
 
-  console.log("new connection: ", clientID)
+  console.log("new connection: ", clientID);
 
   socket.join(clientID); //put the client in a room so they can be reached
 
   //add the current client to the active connections in case they were not added before
-  if(!activeConnections[clientID]) {
-    activeConnections[clientID] = userInfo
+  if (!activeConnections[clientID]) {
+    activeConnections[clientID] = userInfo;
   }
 
   //create a list with all the connected clients except the current one, to be used to track online friends
   const updatedList = {};
   Object.keys(activeConnections).forEach(userID => {
     if (userID !== clientID) {
-      updatedList[userID] = activeConnections[userID]
+      updatedList[userID] = activeConnections[userID];
     }
-  })
+  });
 
   //send this to the client that just joined to update their online friends list
-  socket.emit('updated-friends-list', updatedList)
+  socket.emit('updated-friends-list', updatedList);
 
   //send this to all the clients except the current one to update their friends list
-  socket.broadcast.emit('updated-friends-list', { ...userInfo })
+  socket.broadcast.emit('updated-friends-list', { ...userInfo });
 
   //this listener will be called once the client sends a message to another
   socket.on('send-message', ({ recipient, message }) => {
     socket.to(recipient).emit('receive-message', {
       message,
       sender: clientID
-    })
+    });
     const messageInfo = {
       id: uuidv4(),
       content: message,
@@ -49,27 +49,27 @@ wss.on('connection', socket => {
       receiver_id: recipient,
       is_read: false,
       created_on: (new Date()).toLocaleString("en-us")
-    }
-    conversation[messageInfo.id] = messageInfo
-  })
+    };
+    conversation[messageInfo.id] = messageInfo;
+  });
 
   socket.on('message-read', id => {
     conversation[id].is_read = true;
-    db.updateMessageStatus(id)
-  })
+    db.updateMessageStatus(id);
+  });
 
   socket.on('disconnect', () => {
     delete activeConnections[clientID];
-    console.log("user disconnected: ", clientID)
-    socket.broadcast.emit('remove-user', { ...userInfo }) //to have clients update their online friends list
+    console.log("user disconnected: ", clientID);
+    socket.broadcast.emit('remove-user', { ...userInfo }); //to have clients update their online friends list
     
-    if(Object.keys(conversation).length > 0) {
-      db.updateMessages(conversation)
+    if (Object.keys(conversation).length > 0) {
+      db.updateMessages(conversation);
     }
-  })
-})
+  });
+});
 
 //start the server
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
-})
+});
